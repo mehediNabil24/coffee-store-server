e9554ac3678cf4cf430108ed2f3e757f8badd8bb
mehediNabil <mehedinabil13@gmail.com>
2024-11-29 23:34:27 +0600

    added post at database

--- index.js
+++ index.js
@@ -25,25 +25,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+
+    app.get('/',(req,res)=>{
+        res.send("Coffee server is running")
+    })
+
+
+    app.post('/coffee', async(req,res)=>{
+        const newCoffee = req.body;
+        console.log(newCoffee)
+        const result = await coffeeCollection.insertOne(newCoffee)
+        res.send(result);
+    })
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    app.listen(port,()=>{
+        console.log(`coffee server is running on port ${port}`)
+    })
+  } 
+  catch(err){
+    console.log(err)
   }
 }
-run().catch(console.dir);
+run()
 
 
 
 
 
 
-app.get('/',(req,res)=>{
-    res.send("Coffee server is running")
-})
 
-app.listen(port,()=>{
-    console.log(`coffee server is running on port ${port}`)
-})
+
